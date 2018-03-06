7a4dd0a81bdb5529896e4676d88e52444b0da247
root <root@paul.intelligentcompute.com>
2018-03-06 22:30:26 +0000

    odpulse: added odp multi file writing with 1Mb buffers. avg speed ~760Mb/s

--- odpulse.c
+++ odpulse.c
@@ -2,13 +2,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <odp_api.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define SHM_PKT_POOL_SIZE      (512*2048)
 #define SHM_PKT_POOL_BUF_SIZE  1856
 
 #define NUM_THREADS 4
 #define NUM_INPUT_Q 4
+
+
+#define BUFFER_SIZE 1024*1024
+#define MAX_PACKET_SIZE 1600
+
+
 
 odp_instance_t odp_instance;
 odp_pool_param_t params;
@@ -32,10 +44,33 @@ static void* thread_func(void *arg)
 	int thread_id = *p;
 	time_t now, old = 0;
 
+	int fd;
+	char fname[256] = {0};
+	unsigned char *buf = NULL;
+	unsigned int position = 0;
+
+	sprintf(fname, "%d", thread_id);
+	strcat(fname, "_thread_file");
+	fd = creat(fname, S_IRWXU);
+	if (!fd)
+	{
+		printf("failed to create file. exiting from thread\n");
+		return;
+	}
+
+	printf("init odp thread\n");
 	rv = odp_init_local(odp_instance, ODP_THREAD_WORKER);
 
 	while (1)
         {
+		if (!buf)
+			buf = malloc(BUFFER_SIZE);
+		if (!buf)
+		{
+			printf("failed to allocate ram. exiting from thread\n");
+			return;
+		}
+
 		ev = odp_schedule(NULL, ODP_SCHED_NO_WAIT);
 		pkt = odp_packet_from_event(ev);
 		if (!odp_packet_is_valid(pkt))
@@ -44,6 +79,21 @@ static void* thread_func(void *arg)
 		pkt_len = (int)odp_packet_len(pkt);
 		if (pkt_len)
 		{
+			if (position < BUFFER_SIZE-MAX_PACKET_SIZE)
+			{
+				memcpy(buf+position, odp_packet_l2_ptr(pkt, NULL), pkt_len);
+				position += pkt_len;
+			}
+			if (position >= BUFFER_SIZE-MAX_PACKET_SIZE)
+			{
+				ssize_t written;
+				written = write(fd, buf, position);
+				//printf("#%d have written %d bytes \n", thread_id, written);
+				free(buf);
+				buf = NULL;
+				position = 0;
+			}
+			
 			pkts_cnt++;
 			bytes_cnt += pkt_len;
 			now = time(0);
@@ -94,7 +144,7 @@ int main(int argc, char *argv[])
 
 	odp_pktin_queue_param_init(&pktin_param);
 	pktin_param.op_mode     = ODP_PKTIO_OP_MT;
-	pktin_param.hash_enable = 0;
+	pktin_param.hash_enable = 1;
 	pktin_param.num_queues  = NUM_INPUT_Q;
 	pktin_param.queue_param.sched.sync = ODP_SCHED_SYNC_ATOMIC;
 	pktin_param.queue_param.sched.prio = ODP_SCHED_PRIO_DEFAULT;
